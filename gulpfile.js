e65e7e60f30b013fa5886f3ccf3280d64efb46b3
Blake Winton <bwinton@latte.ca>
2015-01-21 10:41:34 -0500

    Add a gulp file to run cfx, and ignore the profiles.

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var env = process.env;
+var spawn = require('child_process').spawn;
+var gulp = require('gulp');
+
+gulp.task('init', function (cb) {
+  console.log('init');
+  if (!env.CUDDLEFISH_ROOT) {
+    cb('Please run ". bin/activate" in your addon-sdk directory first.');
+    return;
+  }
+  cb();
+});
+
+function getBinary(channel) {
+  return ['-b', '/Applications/Local/FirefoxNightly.app'];
+}
+
+function runCfx(cb, command) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  args = args.concat(getBinary());
+
+  var task = spawn('cfx', args);
+
+  task.stdout.on('data', function (data) {
+    process.stdout.write(data.toString('utf-8'));
+  });
+
+  task.stderr.on('data', function (data) {
+    process.stderr.write(data.toString('utf-8'));
+  });
+
+  task.on('close', function (code) {
+    if (code !== 0) {
+      cb('cfx ' + args[0] + ' exited with code ' + code);
+      return;
+    }
+    cb(code);
+  });
+}
+
+gulp.task('test', ['init'], function(cb) {
+  runCfx(cb, 'test');
+});
+
+gulp.task('package', ['init'], function(cb) {
+  runCfx(cb, 'xpi');
+});
+
+gulp.task('run', ['init'], function(cb) {
+  runCfx(cb, 'run', '-p', 'profile.testing');
+});
+
+gulp.task('default', ['run']);
