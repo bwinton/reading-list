0a6ffed87c30fd6cc8e5dcdfc2b2bc96490d8574
Blake Winton <bwinton@latte.ca>
2015-01-30 14:00:57 -0500

    Add a share button.

--- data/panel-script.js
+++ data/panel-script.js
@@ -1,6 +1,6 @@
 window.addEventListener('click', function (event) {
   var t = event.target;
-  if (t.nodeName == 'IMG') {
+  if (t.nodeName == 'IMG' || t.nodeName == 'LI') {
     self.port.emit('click-icon', t.getAttribute('src'));
   }
 }, false);
