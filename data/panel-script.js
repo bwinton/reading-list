21951227fec8dbadc26b98869700ab3a36bf8ba0
Blake Winton <bwinton@latte.ca>
2015-01-30 14:29:35 -0500

    Tweak styling, mostly by switching to divs.

--- data/panel-script.js
+++ data/panel-script.js
@@ -1,6 +1,8 @@
 window.addEventListener('click', function (event) {
   var t = event.target;
-  if (t.nodeName == 'IMG' || t.nodeName == 'LI') {
+  if (t.nodeName == 'IMG') {
     self.port.emit('click-icon', t.getAttribute('src'));
+  } else if (t.classList.contains('menuitem')) {
+    self.port.emit('click-menu', t.getAttribute('id'));
   }
 }, false);
