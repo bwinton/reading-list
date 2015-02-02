577b9817bbdf5ad81ad6433e98828aa390626c03
Blake Winton <bwinton@latte.ca>
2015-02-02 14:31:24 -0500

    Add icons to the share button’s menu items. And bump the version for release.

--- data/panel-script.js
+++ data/panel-script.js
@@ -1,8 +1,25 @@
+var isMenu = function (target) {
+  while (target != null) {
+    if (target.classList && target.classList.contains('menuitem')) {
+      return target;
+    }
+    target = target.parentNode
+  }
+  return false;
+}
+
+var isIcon = function (target) {
+  if (target.nodeName == 'IMG') {
+    return target;
+  }
+  return false;
+}
+
 window.addEventListener('click', function (event) {
-  var t = event.target;
-  if (t.nodeName == 'IMG') {
-    self.port.emit('click-icon', t.getAttribute('src'));
-  } else if (t.classList.contains('menuitem')) {
-    self.port.emit('click-menu', t.getAttribute('id'));
+  var target;
+  if (target = isMenu(event.target)) {
+    self.port.emit('click-menu', target.getAttribute('id'));
+  } else if (target = isIcon(event.target)) {
+    self.port.emit('click-icon', target.getAttribute('src'));
   }
 }, false);
